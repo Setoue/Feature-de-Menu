6587063e908046f9ca982c01dcece2cc1671c9de
setoue <proshigeru@gmail.com>
2022-09-01 15:36:18 -0300

    Initial Commit

---
+++ ProjetoViewCode/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ProjetoViewCode
+//
+//  Created by user on 01/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .green
+    }
+}
+
